95365c9de8c109b3152efa6ac1cedeb978f11ce8
Ofer Eliassaf <ofer.eliassaf@gmail.com>
2014-01-15 23:16:17 +0200

    implementing few more

--- test/bit2c.js
+++ test/bit2c.js
@@ -6,85 +6,85 @@ var should = require('should'),
       secret: process.env.BIT2C_SECRET
    };
 
-describe('checking bit2c public API', function() {
-   
-   it('should get a ticker of BtcNis', function(done) {
-      this.timeout(10000);
-      bit2c.getTicker('BtcNis', function(error, ticker) {
-         should.not.exist(error);
-         should.exist(ticker.ll); // last price
-         should.exist(ticker.av); // last 24 hours price avarage
-         should.exist(ticker.a);  // last 24 hours volume
-         should.exist(ticker.h);  // highest buy order
-         should.exist(ticker.l);  // lowest sell order
-         done();
-      });
-   });
-
-   it('should get a ticker of LtcBtc', function(done) {
-      this.timeout(10000);
-      bit2c.getTicker('LtcBtc', function(error, ticker) {
-         should.not.exist(error);
-         should.exist(ticker.ll); // last price
-         should.exist(ticker.av); // last 24 hours price avarage
-         should.exist(ticker.a);  // last 24 hours volume
-         should.exist(ticker.h);  // highest buy order
-         should.exist(ticker.l);  // lowest sell order
-         done();
-      });
-   });
-
-   it('should get trades of BtcNis', function(done) {
-      this.timeout(10000);
-      bit2c.getTrades('BtcNis', function(error, ticker) {
-         should.not.exist(error);
-         ticker.length.should.be.above(0);
-         should.exist(ticker[0].date);
-         should.exist(ticker[0].price);
-         should.exist(ticker[0].amount);
-         should.exist(ticker[0].tid);
-         done();
-      });
-   });
-
-   it('should get trades of LtcBtc', function(done) {
-      this.timeout(10000);
-      bit2c.getTrades('LtcBtc', function(error, ticker) {
-         should.not.exist(error);
-         ticker.length.should.be.above(0);
-         should.exist(ticker[0].date);
-         should.exist(ticker[0].price);
-         should.exist(ticker[0].amount);
-         should.exist(ticker[0].tid);
-         done();
-      });
-   });
-
-   it('should get order book of BtcNis', function(done) {
-      this.timeout(10000);
-      bit2c.getOrderBook('BtcNis', undefined, function(error, orderBook) {
-         should.not.exist(error);
-         should.exist(orderBook.bids);
-         should.exist(orderBook.asks);
-         orderBook.bids.length.should.be.above(0);
-         orderBook.asks.length.should.be.above(0);
-         done();
-      });
-   });
-
-   it('should get order book of LtcBtc', function(done) {
-      this.timeout(10000);
-      bit2c.getOrderBook('LtcBtc', undefined, function(error, orderBook) {
-         should.not.exist(error);
-         should.exist(orderBook.bids);
-         should.exist(orderBook.asks);
-         orderBook.bids.length.should.be.above(0);
-         orderBook.asks.length.should.be.above(0);
-         done();
-      });
-   });
-});
-
+// describe('checking bit2c public API', function() {
+//    
+//    it('should get a ticker of BtcNis', function(done) {
+//       this.timeout(10000);
+//       bit2c.getTicker('BtcNis', function(error, ticker) {
+//          should.not.exist(error);
+//          should.exist(ticker.ll); // last price
+//          should.exist(ticker.av); // last 24 hours price avarage
+//          should.exist(ticker.a);  // last 24 hours volume
+//          should.exist(ticker.h);  // highest buy order
+//          should.exist(ticker.l);  // lowest sell order
+//          done();
+//       });
+//    });
+// 
+//    it('should get a ticker of LtcBtc', function(done) {
+//       this.timeout(10000);
+//       bit2c.getTicker('LtcBtc', function(error, ticker) {
+//          should.not.exist(error);
+//          should.exist(ticker.ll); // last price
+//          should.exist(ticker.av); // last 24 hours price avarage
+//          should.exist(ticker.a);  // last 24 hours volume
+//          should.exist(ticker.h);  // highest buy order
+//          should.exist(ticker.l);  // lowest sell order
+//          done();
+//       });
+//    });
+// 
+//    it('should get trades of BtcNis', function(done) {
+//       this.timeout(10000);
+//       bit2c.getTrades('BtcNis', function(error, ticker) {
+//          should.not.exist(error);
+//          ticker.length.should.be.above(0);
+//          should.exist(ticker[0].date);
+//          should.exist(ticker[0].price);
+//          should.exist(ticker[0].amount);
+//          should.exist(ticker[0].tid);
+//          done();
+//       });
+//    });
+// 
+//    it('should get trades of LtcBtc', function(done) {
+//       this.timeout(10000);
+//       bit2c.getTrades('LtcBtc', function(error, ticker) {
+//          should.not.exist(error);
+//          ticker.length.should.be.above(0);
+//          should.exist(ticker[0].date);
+//          should.exist(ticker[0].price);
+//          should.exist(ticker[0].amount);
+//          should.exist(ticker[0].tid);
+//          done();
+//       });
+//    });
+// 
+//    it('should get order book of BtcNis', function(done) {
+//       this.timeout(10000);
+//       bit2c.getOrderBook('BtcNis', undefined, function(error, orderBook) {
+//          should.not.exist(error);
+//          should.exist(orderBook.bids);
+//          should.exist(orderBook.asks);
+//          orderBook.bids.length.should.be.above(0);
+//          orderBook.asks.length.should.be.above(0);
+//          done();
+//       });
+//    });
+// 
+//    it('should get order book of LtcBtc', function(done) {
+//       this.timeout(10000);
+//       bit2c.getOrderBook('LtcBtc', undefined, function(error, orderBook) {
+//          should.not.exist(error);
+//          should.exist(orderBook.bids);
+//          should.exist(orderBook.asks);
+//          orderBook.bids.length.should.be.above(0);
+//          orderBook.asks.length.should.be.above(0);
+//          done();
+//       });
+//    });
+// });
+// 
 describe('checking bit2c private API', function() {
 
    it('should check balance', function(done) {
@@ -135,5 +135,35 @@ describe('checking bit2c private API', function() {
          });
       }, 1000);
    });
+
+   it('should cancel an order', function(done) {
+      this.timeout(10000);
+      bit2c.cancelOrder(credentials, 17, function(error, result) {
+         should.not.exist(error);
+         should.exist(result.OrderResponse);
+         done();
+      });
+   });
+
+   it ('should get last year\'s account history', function(done) {
+      var to = new Date(),
+         from = (new Date()).getTime() - (1000*60*60*24*365);
+
+      this.timeout(10000);
+      bit2c.getAccountHistory(credentials, from, to, function(error, result) {
+         should.not.exist(error);
+
+         should.exist(result);
+         result.should.be.an.instanceOf(Array);
+         console.log(result);
+         done();
+      });
+   });
+
+
+
+
+
+
 });
 
